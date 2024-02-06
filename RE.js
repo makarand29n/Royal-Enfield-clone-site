b154ab41e166c97e0270d24b6935e2620e442e66
makarand29n <60806236+makarand29n@users.noreply.github.com>
2024-02-06 17:49:19 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,27 @@
+function a(){
+    document.getElementById("pp").src="./images/continental-gt-home-motorcycle2.avif"
+}
+function b(){
+    document.getElementById("pp").src="./images/interceptor-home-motorcycle.avif"
+}
+function c(){
+    document.getElementById("pp").src="./images/royal-enfield-meteor-new-banner8.avif"
+}
+function d(){
+    document.getElementById("pp").src="./images/hunter-350-motorcycle-home4.avif"
+}
+function e(){
+    document.getElementById("pp").src="./images/royal-enfield-scram-411-home-motorcycle5.avif"
+}
+function f(){
+    document.getElementById("pp").src="./images/classic-350-motorcycle-section7.avif"
+}
+function g(){
+    document.getElementById("pp").src="./images/royal-enfield-meteor-new-bannermeteor 350.avif"
+}
+function h(){
+    document.getElementById("pp").src="./images/sleethblack-banner-des9.avif"
+}
+function i(){
+    document.getElementById("pp").src="./images/banner10.avif"
+}
